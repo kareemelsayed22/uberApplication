0819de8252f57e9bd3d0d75c9ff2c160676c2a5c
yasmeenmunasser <yasmeen.munasser88@qmail.cuny.edu>
2020-11-02 13:04:55 -0500

    Setup usermodel & usercontroller with postgres database

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -32,14 +32,23 @@ function destroy() {
 
 }
 async function create() {
-  const kareem = User.build({ username: "Kareem", password: "abcd" });
-
-  console.log(kareem instanceof User); // true
-  console.log(kareem.username); // "Jane"
-
-  await kareem.save();
+  // async function recreateUserTable(){
+  //   const database = await User.sync({ force: true });
+  //   console.log("The table for the User model was just (re)created!");
+  //   return database
+  // }
+  // recreateUserTable().then(() => {
+  //     const kareem = User.create({ username: "Kareem", password: "abcd" });
+  //     console.log('Kareem was saved to the database!');
+  // })
+  const kareem = User.create({ username: "Kareem", password: "abcd" });
   console.log('Kareem was saved to the database!');
+
+
+
+
 }
+
 create();
 module.exports = {
   index
--- src/models/user.js
+++ src/models/user.js
@@ -23,17 +23,22 @@ const User = sequelize.define('User', {
 }, {
   // Other model options go here
   sequelize, // We need to pass the connection instance
-  modelName: 'User' // We need to choose the model name
+  modelName: 'User', // We need to choose the model name
+
+  // disable the modification of table names; By default, sequelize will automatically
+  // transform all passed model names (first parameter of define) into plural.
+  // if you don't want that, set the following
+  // freezeTableName: true
 });
 
 // `sequelize.define` also returns the model
-console.log(User === sequelize.models.User); // true
+// console.log(User === sequelize.models.User); // true
 // w/e you use await use an async function
-async function recreateUserTable(){
-  await User.sync({ force: true });
-  console.log("The table for the User model was just (re)created!");
-}
-recreateUserTable();
+// async function recreateUserTable(){
+//   await User.sync({ force: true });
+//   console.log("The table for the User model was just (re)created!");
+// }
+// recreateUserTable();
 
 
 // class User {
